43693b2ac97bb48176094a410cbd19f5c0d99edb
QBOMONTI\Sebastian <ayk@quantheque.com>
2021-03-29 22:30:31 +0300

    Shop And Buttons Set Up

--- Dungeon Escape Game/Assets/Scripts/Shop/ShopScript.cs
+++ Dungeon Escape Game/Assets/Scripts/Shop/ShopScript.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class ShopScript : MonoBehaviour
@@ -7,22 +8,45 @@ public class ShopScript : MonoBehaviour
 
     public GameObject _shopPanel;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-     void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "PlayerGameObject")
         {
+            PlayerScript player = collision.GetComponent<PlayerScript>();
+            if ( player != null)
+            {
+                UIManagerScript.Instance.OpenShop(player._diamondAmount);
+            }
             _shopPanel.SetActive(true);
         }
     }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "PlayerGameObject")
+        {
+            _shopPanel.SetActive(false);
+        }
+    }
+
+    public void SelectItem(int item)
+    {
+        switch (item)
+        {
+            case 0: 
+        UIManagerScript.Instance.UpdateShopSelection(62);
+        break;
+            case 1:
+                UIManagerScript.Instance.UpdateShopSelection(-35);
+                break;
+            case 2:
+                UIManagerScript.Instance.UpdateShopSelection(-135);
+                break;
+
+        }
+
+    }
 }
+
---
+++ Dungeon Escape Game/Assets/Scripts/UI/UIManagerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManagerScript : MonoBehaviour
+{
+    private static UIManagerScript _instance;
+    public static UIManagerScript Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.Log("null");
+            }
+            return _instance;
+        }
+    }
+
+    public Text _playerGamCountText;
+    public Image _selectionSlider;
+
+   
+
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+    public void UpdateShopSelection(int yPosition)
+    {
+        _selectionSlider.rectTransform.anchoredPosition = new Vector2(_selectionSlider.rectTransform.anchoredPosition.x, yPosition);
+    }
+
+
+    public void OpenShop(int gemCount)
+    {
+        _playerGamCountText.text = "" + gemCount + "G";
+    }
+}
